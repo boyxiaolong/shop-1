f6d8cc39b957af97d0995fabbe8d5ddeb6a636f8
Alexandru <oat.alexandru@gmail.com>
2015-09-08 23:02:03 +0300

    change static_root in settings

--- shop/settings.py
+++ shop/settings.py
@@ -134,13 +134,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = ''
-
-STATIC_URL = '/static/'
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
